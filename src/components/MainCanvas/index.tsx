93b0da253d2f438f14dd02dbf2e5e1913d9af59e
koke-desu <gbpvp25645@yahoo.co.jp>
2023-08-03 00:26:56 +0900

    wip

--- src/components/MainCanvas/index.tsx
+++ src/components/MainCanvas/index.tsx
@@ -2,45 +2,70 @@
 import React, { useEffect, useRef, useState } from "react";
 type Props = {};
 
+type Element = { id: string; type: "rect"; width: number; height: number; x: number; y: number };
+
 const MainCanvas: React.FC<Props> = ({}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [currentMouse, setCurrentMouse] = useState({ x: 0, y: 0 });
   const [isMouseDown, setIsMouseDown] = useState(false);
-  const [downMouse, setDownMouse] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const context = canvasRef.current?.getContext("2d");
-    if (!context) return;
-
-    context.fillStyle = "red";
-    context.fillRect(100, 100, 300, 300);
-  }, []);
+  const [elements, setElements] = useState<Element[]>([]);
+  const [currentElementId, setCurrentElementId] = useState("");
 
   const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    console.log("mouse down");
     setIsMouseDown(true);
-    setDownMouse({ x: e.clientX, y: e.clientY });
+    const id = new Date().getTime().toString();
+    const element: Element = {
+      id,
+      type: "rect",
+      width: 0,
+      height: 0,
+      x: e.clientX,
+      y: e.clientY,
+    };
+    setElements([...elements, element]);
+    setCurrentElementId(id);
   };
 
   const onMouseUp = (e: React.MouseEvent<HTMLCanvasElement>) => {
     console.log("mouse up");
     setIsMouseDown(false);
+
+    const currentElement = elements.find((element) => element.id === currentElementId);
+    if (!currentElement) return;
+    if (currentElement.height === 0 || currentElement.width === 0) {
+      setElements(elements.filter((element) => element.id !== currentElementId));
+    }
   };
 
   const onMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    console.log("mouse move");
-    setCurrentMouse({ x: e.clientX, y: e.clientY });
-
     if (!isMouseDown) return;
-    const context = canvasRef.current?.getContext("2d");
-    if (!context) return;
-    context.rect(
-      downMouse.x,
-      downMouse.y,
-      currentMouse.x - downMouse.x,
-      currentMouse.y - downMouse.y
+
+    setElements(
+      elements.map((element) => {
+        if (element.id !== currentElementId) return element;
+        return {
+          ...element,
+          width: e.clientX - element.x,
+          height: e.clientY - element.y,
+        };
+      })
     );
   };
+
+  const render = () => {
+    const context = canvasRef.current?.getContext("2d");
+    if (!context) return;
+
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    elements.forEach((element) => {
+      context.fillStyle = "red";
+      context.fillRect(element.x, element.y, element.width, element.height);
+    });
+  };
+
+  useEffect(() => {
+    console.log("update");
+    render();
+  }, [elements]);
 
   return (
     <canvas
