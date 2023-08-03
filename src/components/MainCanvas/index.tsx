79057281595378ec467010940b4a9fd6d82302ac
koke-desu <gbpvp25645@yahoo.co.jp>
2023-08-03 21:47:46 +0900

    wip

--- src/components/MainCanvas/index.tsx
+++ src/components/MainCanvas/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 type Props = {};
 
@@ -9,6 +10,7 @@ const MainCanvas: React.FC<Props> = ({}) => {
   const [isMouseDown, setIsMouseDown] = useState(false);
   const [elements, setElements] = useState<Element[]>([]);
   const [currentElementId, setCurrentElementId] = useState("");
+  const [scale, setScale] = useState(1);
 
   const onMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
     setIsMouseDown(true);
@@ -51,32 +53,77 @@ const MainCanvas: React.FC<Props> = ({}) => {
     );
   };
 
-  const render = () => {
-    const context = canvasRef.current?.getContext("2d");
-    if (!context) return;
+  useEffect(() => {
+    const render = () => {
+      const context = canvasRef.current?.getContext("2d");
+      if (!context) return;
 
-    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    elements.forEach((element) => {
-      context.fillStyle = "red";
-      context.fillRect(element.x, element.y, element.width, element.height);
-    });
-  };
+      context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+      context.scale(scale, scale);
+      elements.forEach((element) => {
+        context.fillStyle = "red";
+        context.fillRect(element.x, element.y, element.width, element.height);
+      });
+
+      // 描画の拡大率をもとに戻す
+      context.scale(1 / scale, 1 / scale);
+    };
+
+    render();
+
+    const interval = setInterval(() => {
+      console.log("update");
+      render();
+    }, 1000 / 30);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [elements, scale]);
 
   useEffect(() => {
-    console.log("update");
-    render();
-  }, [elements]);
+    const onWheel = (e: WheelEvent) => {
+      if (!e.ctrlKey) return;
+
+      e.preventDefault();
+      setScale((scale) => {
+        const scrollAmount = e.deltaY;
+        const scaleAmount = 0.1;
+        const newScale = scale + (scrollAmount > 0 ? -scaleAmount : scaleAmount);
+
+        if (newScale >= 0.01 && newScale <= 10) {
+          return newScale;
+        }
+
+        return scale;
+      });
+    };
+    document.addEventListener("wheel", onWheel, { passive: false });
+
+    return () => {
+      document.removeEventListener("wheel", onWheel);
+    };
+  }, [scale]);
 
   return (
-    <canvas
-      height={window.innerHeight}
-      width={window.innerWidth}
-      ref={canvasRef}
-      className="absolute top-0 left-0 z-0 bg-gray-200"
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
-    ></canvas>
+    <>
+      <Head>
+        <title>Canvas</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        ></meta>
+      </Head>
+      <canvas
+        height={window.innerHeight}
+        width={window.innerWidth}
+        ref={canvasRef}
+        className="absolute top-0 left-0 z-0 bg-gray-200"
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
+      ></canvas>
+    </>
   );
 };
 export default MainCanvas;
